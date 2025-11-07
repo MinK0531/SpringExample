073e9774e7afe4f67a66cd1054cb86c6951e2b6f
min <rlaalsrud20807@naver.com>
2025-11-07 17:19:18 +0900

    [김민경] -spring예제 프로젝트 생성

---
+++ src/main/java/com/mink/springexamlpe/SpringExamlpeApplication.java
@@ -0,0 +1,16 @@
+package com.mink.springexamlpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExamlpeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringExamlpeApplication.class, args);
+    }
+
+}
+
+
+
