c4e140056bd54bea2c61ebe14c2f1e833d605ae8
min <rlaalsrud20807@naver.com>
2025-11-13 18:23:02 +0900

    [김민경] -입력과 조회 Cycle

---
+++ src/main/java/com/mink/springexamlpe/mvc/service/UserService.java
@@ -0,0 +1,30 @@
+package com.mink.springexamlpe.mvc.service;
+
+import com.mink.springexamlpe.mvc.repository.UserRepository;
+import com.mink.springexamlpe.mvc.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // 가장 최근에 등록된 사용자 정보 얻어오기
+    public User getLastUser(){
+
+        // new_user 테이블의 가장 최근 추가된 행 조회
+        User user = userRepository.selectLastUser();
+        return user;
+    }
+
+    public int createUser(
+            String name
+            , String birthday
+            , String email
+            , String introduce){
+        int count = userRepository.insertUser(name, birthday, email, introduce);
+        return count;
+    }
+}
